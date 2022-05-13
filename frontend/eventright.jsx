871371d507158c41dfe8fe40d26b9573c751c8dd
Andrew <aaayin@gmail.com>
2022-05-13 18:23:57 -0400

    Adds Static Pages

---
+++ frontend/eventright.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+window.addEventListener('DOMContentLoaded', ()=> {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h2>Natty transformation no cyap</h2>, root )
+});
