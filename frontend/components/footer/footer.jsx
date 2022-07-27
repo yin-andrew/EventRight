29e1c9f8d63d6a1959d54fea74ce364ce94ff417
Andrew Yin <aaayin@gmail.com>
2022-07-26 22:40:38 -0700

    Update footer.jsx

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,11 +5,11 @@ function Footer(props) {
     return(
         <div className="footer-wrapper">
             <div className="footer-links-container">
-                <a className="personal-links" href="https://github.com/yin-andrew/EventRight">
+                <a className="personal-links" target="_blank" rel="noopener noreferrer" href="https://github.com/yin-andrew/EventRight">
                     <FaGithub /> Github
                 </a>
                 <div className="link-space"></div>
-                <a className="personal-links" href="https://www.linkedin.com/in/andrewyin16407/">
+                <a className="personal-links" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/andrewyin16407/">
                     <FaLinkedin /> LinkedIn
                 </a>
             </div>
